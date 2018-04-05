6630f83cd22b287e2d084e05bf49fbba4205eca4
rrioja <rodolfo@geoslab.com>
2018-04-05 17:37:24 +0200

    añado icono al index.html

--- src/main/java/org/sitmun/app/territorial/Application.java
+++ src/main/java/org/sitmun/app/territorial/Application.java
@@ -4,10 +4,6 @@ import org.sitmun.plugin.core.annotation.SitmunApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-
-
-//@Import(SpringDataRestConfiguration.class)
 @SitmunApplication
 @ComponentScan("org.sitmun.plugin.demo")
 public class Application {
