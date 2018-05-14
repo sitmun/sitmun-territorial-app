292006f0adba14567c107b929266d23c54ca9278
rbejar <rbejar@unizar.es>
2018-05-14 12:29:14 +0200

    Improved build scripts to support versioning

--- build-scripts/install-dependencies.sh
+++ build-scripts/install-dependencies.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 if [ -z $PLUGIN_DIR ]; then
-    PLUGIN_DIR=$(mktemp -d)
+    export PLUGIN_DIR=$(mktemp -d)
 fi
 
 echo
 echo "Install dependencies script ..."
 echo
 
-if git clone https://github.com/sitmun/sitmun-plugin-core.git $PLUGIN_DIR/sitmun-plugin-core; then
-    cd $PLUGIN_DIR/sitmun-plugin-core
-    ./gradlew clean install
-else
+if ./gradlew cloneDependencyRepos; then
     echo
-    echo "Install dependencies script FAILED"
+    echo "Repos cloned ..."
     echo
-    exit 1
-fi
-
-if git clone https://github.com/sitmun/sitmun-plugin-demo.git $PLUGIN_DIR/sitmun-plugin-demo; then
-    cd $PLUGIN_DIR/sitmun-plugin-demo
-    ./gradlew clean install
+    for D in $PLUGIN_DIR/*/; do cd ${D}; ./build-scripts/install-dependencies.sh; ./gradlew clean install; done
 else
     echo
     echo "Install dependencies script FAILED"
