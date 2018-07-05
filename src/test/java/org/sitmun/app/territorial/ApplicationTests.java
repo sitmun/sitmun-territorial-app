5773b0da76fa40e05fe25140eafb6bb16ec6f087
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2018-07-05 19:03:03 +0200

    Applied coding style guidelines

--- src/test/java/org/sitmun/app/territorial/ApplicationTests.java
+++ src/test/java/org/sitmun/app/territorial/ApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+  @Test
+  public void contextLoads() {
+  }
 }
