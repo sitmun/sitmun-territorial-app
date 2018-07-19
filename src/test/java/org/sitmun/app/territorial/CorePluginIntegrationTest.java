56f5e8cc2778406d7c403dd4eba1094e298ec1d1
rrioja <rodolfo@geoslab.com>
2018-07-19 14:51:25 +0200

    test fix

--- src/test/java/org/sitmun/app/territorial/CorePluginIntegrationTest.java
+++ src/test/java/org/sitmun/app/territorial/CorePluginIntegrationTest.java
@@ -29,7 +29,7 @@ public class CorePluginIntegrationTest {
     public void testCoreAPI() throws Exception {
         mvc.perform(get("/api/users")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(401))
+                .andExpect(status().isOk())
                 ;
     }
 }
