8153f61ee4e010a19efdb01948c14af3011123d2
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2018-08-26 17:07:50 +0200

    Added stactrace

--- build-scripts/script.sh
+++ build-scripts/script.sh
@@ -4,7 +4,7 @@ echo "Building script ..."
 echo
 
 cd $TRAVIS_BUILD_DIR
-if ./gradlew clean assemble check --info; then
+if ./gradlew clean assemble check --info --stacktrace; then
     echo
 else
     echo
